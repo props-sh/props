cf9f56b68576009988edf0f8c4317a93b9bb4175
Mihai Bojin <mihai.bojin@gmail.com>
2021-10-28 11:56:50 +0100

    Use git 2.9 core.hooksPath for running git hooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,24 +5,3 @@ plugins {
 
 group = "sh.props"
 version = project.version
-
-// install git hooks
-tasks.register<Exec>("gitHooks") {
-    dependsOn("installGitHooks")
-
-    description = "Ensures the project's git hooks are installed and executable"
-    commandLine("chmod", "-R", "+x", "./.git/hooks/")
-}
-tasks.register<Copy>("installGitHooks") {
-    description = "Installs the project's git hooks"
-
-    doLast {
-        copy {
-            from("./githooks/") {
-                include("*")
-            }
-            into("./.git/hooks")
-        }
-    }
-}
-
