d52fc2b1e5f358dc5d1213a89f89ed541e52c123
Mihai Bojin <mihai.bojin@gmail.com>
2021-11-03 15:24:21 +0000

    Defined base Prop types

---
+++ java-props-core/src/main/java/sh/props/base/AbstractInstantProp.java
@@ -0,0 +1,42 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Mihai Bojin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package sh.props.base;
+
+import java.time.Instant;
+import sh.props.CustomProp;
+import sh.props.converter.InstantConverter;
+import sh.props.interfaces.Prop;
+
+/**
+ * Helper class meant to act as a base class when defining a {@link Prop} with the underlying type.
+ */
+public abstract class AbstractInstantProp extends CustomProp<Instant> implements InstantConverter {
+
+  protected AbstractInstantProp(
+      String key, Instant defaultValue, String description, boolean isRequired, boolean isSecret) {
+    super(key, defaultValue, description, isRequired, isSecret);
+  }
+}
