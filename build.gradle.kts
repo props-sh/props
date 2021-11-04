b403c29321fd9d7f13922f4b043d62e5479db212
Mihai Bojin <mihai.bojin@gmail.com>
2021-11-04 21:01:46 +0000

    Allow using dependencies from the local Maven cache (#34)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,17 @@ version = project.version
 
 buildscript {
     repositories {
+        // uncomment if you need to use the local Maven cache
+        // mavenLocal()
         mavenCentral()
     }
     dependencies {
         "classpath"(group = "com.google.googlejavaformat", name = "google-java-format", version = "1.10.0")
+    }
+    allprojects {
+        repositories {
+        // uncomment if you need to use the local Maven cache
+        // mavenLocal()
+        }
     }
 }
