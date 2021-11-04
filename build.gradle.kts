136984ab7a9657d168493ce369fbd743caab234c
Mihai Bojin <mihai.bojin@gmail.com>
2021-11-04 20:51:28 +0000

    Allow loading local Maven artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,15 @@ version = project.version
 
 buildscript {
     repositories {
+        mavenLocal()
         mavenCentral()
     }
     dependencies {
         "classpath"(group = "com.google.googlejavaformat", name = "google-java-format", version = "1.10.0")
+    }
+    allprojects {
+        repositories {
+            mavenLocal()
+        }
     }
 }
